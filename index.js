65fbe00e248e5d2b9244766171c09b2d82065253
ABINEL MWANGI <kiraguabinel@outlook.com>
2023-03-28 16:04:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = (num1 * num2);
 
-// Write your code here
+//console.log(multiply)
+const random = Math.floor(Math.random(0.1)*10)
+
+const num3 = 24;
+const num4 = 5;
+const mod = (num3 % num4);
+
+const max = Math.max(12, 3, 5, 18, 20);
